805550768bac77a9c483a2db356c305be164d3a3
Rusak21 <sg970465@gmail.com>
2024-04-12 00:20:32 +0200

    base controll

---
+++ Assets/Source/Core/Controllers/CubeController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    public GameObject Cube;
+
+    private void Start()
+    {
+        Cube = GameObject.Find("Cube1");
+    }
+
+    [SerializeField]
+    private const float SPEED = 1.0f;
+    private const float DELTA_TIME = 0.01f;
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+            Cube.transform.Translate(DELTA_TIME * SPEED * Vector3.forward);
+
+        if (Input.GetKey(KeyCode.A))
+            Cube.transform.Translate(DELTA_TIME * SPEED * Vector3.left);
+
+        if (Input.GetKey(KeyCode.S))
+            Cube.transform.Translate(DELTA_TIME * SPEED * Vector3.back);
+
+        if (Input.GetKey(KeyCode.D))
+            Cube.transform.Translate(DELTA_TIME * SPEED * Vector3.right);
+    }
+}
---
+++ Assets/Source/Core/Utils/Util.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Util : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("hi");
+    }
+}
