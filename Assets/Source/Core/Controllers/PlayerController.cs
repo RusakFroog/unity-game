0c946858896075142d0307791ee4fd9afdb50103
Rusak21 <sg970465@gmail.com>
2024-04-18 00:49:05 +0200

    added player anim, added prefabs

--- Assets/Source/Core/Controllers/PlayerController.cs
+++ Assets/Source/Core/Controllers/PlayerController.cs
@@ -6,37 +6,55 @@ public class PlayerController : MonoBehaviour
     private float _moveSpeed = 5.0f;
 
     [SerializeField]
-    private DynamicJoystick _joystick;
+    private FloatingJoystick _joystick;
     
     private PlayerController _player;
-    private Vector3 _cameraOffset;
+    private Vector3 _cameraOffsetPosition;
     private Rigidbody _rigidbody;
     private Animator _animator;
+
+    //[SerializeField]
+    //private GameObject _gameObjectSetup;
 
     private void Awake()
     {
         _player = this;
-        _cameraOffset = Camera.main.transform.position;
+
+        _cameraOffsetPosition = Camera.main.transform.position;
+
         _rigidbody = _player.GetComponent<Rigidbody>();
         _animator = _player.GetComponent<Animator>();
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
-        Vector3 velocity = new Vector3(_joystick.Horizontal, _rigidbody.velocity.y, _joystick.Vertical) * _moveSpeed;
+        _movePlayer();
+    }
 
-        _rigidbody.velocity = velocity;
+    private void _movePlayer()
+    {
+        bool isMoving = _joystick.Horizontal <= -0.25f || _joystick.Vertical <= -0.25f || _joystick.Horizontal >= 0.25f || _joystick.Vertical >= 0.25f;
 
-        Camera.main.transform.position = new Vector3(_player.transform.position.x + _cameraOffset.x, _cameraOffset.y, _player.transform.position.z + _cameraOffset.z);
-
-        if (_joystick.Horizontal != 0 || _joystick.Vertical != 0)
+        if (isMoving)
         {
-            _rigidbody.rotation = Quaternion.LookRotation(_rigidbody.velocity);
-            
-            Quaternion newRotation = new Quaternion(0f, _rigidbody.rotation.y, 0f, _rigidbody.rotation.w);
-            newRotation.Normalize();
+            Vector3 velocity = new Vector3(_joystick.Horizontal, _rigidbody.velocity.y, _joystick.Vertical) * _moveSpeed;
 
-            _rigidbody.rotation = newRotation;
+            _rigidbody.velocity = velocity;
+            _rigidbody.rotation = Quaternion.LookRotation(_rigidbody.velocity);
+            _rigidbody.rotation = new Quaternion(0f, _rigidbody.rotation.y, 0f, _rigidbody.rotation.w);
+
+            _moveCamera();
         }
+        else
+        {
+            _rigidbody.velocity = new Vector3(0, 0, 0); ;
+        }
+
+        _animator.SetBool("IsRunning", isMoving);
+    }
+
+    private void _moveCamera()
+    {
+        Camera.main.transform.position = new Vector3(_player.transform.position.x + _cameraOffsetPosition.x, _cameraOffsetPosition.y, _player.transform.position.z + _cameraOffsetPosition.z);
     }
 }
