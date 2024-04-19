00bdbd0b775894f0e832336476e40e6eeef8cc4e
Rusak21 <sg970465@gmail.com>
2024-04-19 17:48:36 +0200

    bake shadows, add fps debug

---
+++ Assets/Source/Core/UI/FpsManager.cs
@@ -0,0 +1,31 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Assets.Source.Core.UI
+{
+    public class FpsManager : MonoBehaviour
+    {
+        public float UpdateTimer = 0.2f;
+
+        private TextMeshProUGUI _fpsCounter;
+        private float _updateTime;
+
+        private void Awake()
+        {
+            Application.targetFrameRate = 120;
+
+            _fpsCounter = GetComponent<TextMeshProUGUI>();
+        }
+
+        private void Update()
+        {
+            _updateTime -= Time.deltaTime;
+
+            if (_updateTime < 0)
+            {
+                _fpsCounter.text = "FPS: " + ((int)(1f / Time.smoothDeltaTime)).ToString();
+                _updateTime = UpdateTimer;
+            }
+        }
+    }
+}
